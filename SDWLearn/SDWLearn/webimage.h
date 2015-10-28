8209a1e3f093f23b43cf1631cef4dabe9415e059
guopenghao <guopenghao@haogw.com>
2015-10-28 09:56:55 +0800

    add web image

---
+++ SDWLearn/SDWLearn/webimage.h
@@ -0,0 +1,13 @@
+//
+//  webimage.h
+//  SDWLearn
+//
+//  Created by Apple on 15/10/28.
+//  Copyright © 2015年 gph. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface webimage : NSObject
+
+@end
---
+++ SDWLearn/SDWLearn/webimage.m
@@ -0,0 +1,13 @@
+//
+//  webimage.m
+//  SDWLearn
+//
+//  Created by Apple on 15/10/28.
+//  Copyright © 2015年 gph. All rights reserved.
+//
+
+#import "webimage.h"
+
+@implementation webimage
+
+@end
