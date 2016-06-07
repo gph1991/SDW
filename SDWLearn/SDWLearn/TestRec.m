c3ebfb80da6f74265c849ae9a04599baff925aec
guopenghao <guopenghao@haogw.com>
2016-06-07 18:44:12 +0800

    comment

--- SDWLearn/SDWLearn/TestRec.m
+++ SDWLearn/SDWLearn/TestRec.m
@@ -16,13 +16,17 @@
 {
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);//填充颜色
-    CGContextMoveToPoint(context, 160, 180);
-    CGContextAddArc(context, 50, 50, 30, 0, M_PI_2*3 , 0);
+//    CGContextMoveToPoint(context, 160, 180);
+    CGContextAddArc(context, 50, 50, 30, 0, M_PI_2 , 0);
     
     /*
+     
+     For example, on iOS,a UIView flips the Y-coordinate by scaling the Y values by -1. In a flipped coordinate system, specifying a clockwise arc results in a counterclockwise arc after the transformation is applied
+     
      CGContextAddArc属于CG类库
-     所以最终的结果要沿X轴进行翻转(y=y*-1，这种情况下，M_PI_2在上部)
-     或者简单起见1表示逆时针，0表示顺时针。
+     有两种方式 1.M_PI_2在上部，0为逆时针。最终的结果要进行翻转(y=y*-1)变为UIView的坐标系
+     2.M_PI_2在下部，0为顺时针。无需额外处理
+     
      UIBezierPath 属于UIKit。结果无需额外处理。M_PI_2在下部
      */
 //    CGContextClosePath(context);
